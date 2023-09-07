5831192261069e6cc0f73932f549109dada86030
RicardoEgas <rj_egas@hotmail.com>
2023-09-07 03:36:21 +0200

    Refactor code

--- src/redux/players.js
+++ src/redux/players.js
@@ -1,20 +1,15 @@
-// src/redux/players.js
-
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const perPage = 100; // Set the number of results per page
 const maxRetries = 5; // Increase the maximum number of retries
-let retryDelay = 1000; // Initial delay in milliseconds (1 second)
 
-// Define an initial state
 const initialState = {
   data: [],
   status: 'idle',
   error: null,
 };
 
-// Define an async thunk to fetch player data with pagination and retry logic
 export const fetchPlayers = createAsyncThunk('players/fetchPlayers', async () => {
   let allPlayers = [];
   let page = 1;
@@ -37,13 +32,10 @@ export const fetchPlayers = createAsyncThunk('players/fetchPlayers', async () =>
       allPlayers = [...allPlayers, ...response.data.data];
       page += 1; // Increment page for the next request
     } catch (error) {
-      console.error('Request failed:', error);
       retries += 1;
 
-      // Use exponential backoff with a maximum delay of 30 seconds (30000 milliseconds)
-      retryDelay = Math.min(retryDelay * 2, 30000);
       // eslint-disable-next-line no-await-in-loop
-      await new Promise((resolve) => setTimeout(resolve, retryDelay));
+      await new Promise((resolve) => setTimeout(resolve, 30000));
     }
   }
 
